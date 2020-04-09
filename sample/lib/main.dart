51617330d955082b09e1e55a90f489baa6b0f763
tnovoselec <tomislav.novoselec@gmail.com>
2020-04-09 21:54:10 +0200

    sample project

--- sample/lib/main.dart
+++ sample/lib/main.dart
@@ -6,25 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
@@ -35,10 +22,26 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: IndefiniteProgressIndicator(
-        activeColor: Colors.green,
-        radius: 45,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IndefiniteProgressIndicator(
+              activeColor: Colors.red,
+              radius: 45,
+            ),
+            IndefiniteProgressIndicator(
+              activeColor: Colors.blue,
+              radius: 25,
+            ),
+            IndefiniteProgressIndicator(
+              activeColor: Colors.greenAccent,
+              radius: 125,
+            ),
+          ],
+        ),
       ),
     );
   }
